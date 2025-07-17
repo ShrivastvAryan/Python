7313ebb0d07b34d8a29701bc8b851779cdf5b726
Aryan Shrivastava <asguy29@gmail.com>
2025-07-17 16:44:37 +0530

    first push

---
+++ Python/first.py
@@ -0,0 +1,10 @@
+# First code
+print("Hello world \n\"how are you\"")
+
+'''
+This is a multi line comment
+'''
+
+print('Hey',6,7,sep='-',end="009\n")
+
+#Universal sep is space 
---
+++ Python/second.py
@@ -0,0 +1,20 @@
+a=1
+print(a)
+b="Aryan" #String data type
+c=True #Boolean,(Value of True is 1)
+print(b)
+print(type(a))
+
+print(a+c) #For this the data type must be same
+
+list1=[0,4,5,[-4,5],["apple","banana"]] #Mutable 
+print(list1)
+
+tuple1=(0,4,5,(4,5),"Aryan") #immutable
+print(tuple1)
+
+dict1={"Name":"Aryan","age":28,"canVote":True}
+
+print(dict1)
+
+#Python mai sab kuch ek object hota hai
