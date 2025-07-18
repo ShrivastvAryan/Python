8e21620907a70e642773d376d585fef71c282d74
Aryan Shrivastava <asguy29@gmail.com>
2025-07-18 20:03:21 +0530

    updated

---
+++ Python/tenth.py
@@ -0,0 +1,29 @@
+#Ex1
+for i in range(12):
+    print(i)
+    if(i==10):
+        break
+
+print('Loop ko chorkar nikal gya')
+
+#Ex2
+
+for i in range(12):
+
+    if(i==10): 
+        break
+
+    print(i)
+
+#Ex3
+
+print('Loop ko chorkar nikal gya')
+
+for i in range(12):
+
+    if(i==10): 
+        continue
+
+    print(i)
+
+print('Iteration skip hoga isme')
